e810fefa9ca8baa7de3734b6b92849ec8ca5a9a3
Vishnu Teja <51827179+viz-nu@users.noreply.github.com>
2022-07-30 10:46:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const userdata=[
+    {
+        name:"viz",
+        calendar: [
+        { slot: "00:00 - 00:30 ", availability: true },
+        { slot: "00:30 - 01:00 ", availability: true },
+        { slot: "01:00 - 01:30 ", availability: true },
+        { slot: "01:30 - 02:00 ", availability: true },
+        { slot: "2:00  - 2:30 ", availability: true },
+        { slot: "2:30  - 3:00 ", availability: true },
+        { slot: "3:00  - 3:30 ", availability: true },
+        { slot: "3:30  - 4:00 ", availability: true },
+        { slot: "4:00  - 4:30 ", availability: true },
+        { slot: "4:30  - 5:00 ", availability: true },
+        { slot: "5:00  - 5:30 ", availability: true },
+        { slot: "5:30  - 6:00 ", availability: true },
+        { slot: "6:00  - 6:30 ", availability: true },
+        { slot: "6:30  - 7:00 ", availability: true },
+        { slot: "7:00  - 7:30 ", availability: true },
+        { slot: "7:30  - 8:00 ", availability: true },
+        { slot: "8:00  - 8:30 ", availability: true },
+        { slot: "8:30  - 9:00 ", availability: true },]
+    
+
+    }
+]
